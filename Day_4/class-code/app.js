fe1d7be57f3f58cd4b7e67d5c41ee4f5bcdcb305
Mikael von Ketelhodt <vonketelhodtm@gmail.com>
2020-07-09 16:54:53 +0200

    Bug fixed

--- Day_4/class-code/app.js
+++ Day_4/class-code/app.js
@@ -11,20 +11,20 @@ usernameInputElement.addEventListener("keyup", async (e) => {
   const baseUrl = "https://api.github.com/users/" + username;
   console.log(baseUrl);
 
-    fetch(baseUrl)
-      .then(function (response) {
-        response
-          .json()
-          .then((userData) => {
-            console.log("User Location: ", userData.location);
-          })
-          .catch((err) => {
-            console.log("Aww...", err);
-          });
-      })
-      .catch((err) => {
-        console.log("Aww...", err);
-      });
+    // fetch(baseUrl)
+    //   .then(function (response) {
+    //     response
+    //       .json()
+    //       .then((userData) => {
+    //         console.log("User Location: ", userData.location);
+    //       })
+    //       .catch((err) => {
+    //         console.log("Aww...", err);
+    //       });
+    //   })
+    //   .catch((err) => {
+    //     console.log("Aww...", err);
+    //   });
 
   try {
     const response = await fetch(baseUrl);
